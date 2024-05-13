328a1a823d9a551976edeeca2e3341f5b6abd414
Sachinthana Buddhika <112784979+CydexCode@users.noreply.github.com>
2024-05-13 05:31:07 +0530

    final

--- DependencyInjectionDemo.Shared/IBlogService.cs
+++ DependencyInjectionDemo.Shared/IBlogService.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DependencyInjectionDemo.Models;
 
 namespace DependencyInjectionDemo.Shared
 {
-    internal interface IBlogService
+    public interface IBlogService
     {
+        public List<Post> GetAllPosts();
+        public Post GetPost(int id);
     }
+
 }
